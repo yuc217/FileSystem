a39ae363d6338539993c4ccbdc17dcd16439e799
Yuchen Cai <yuchencai@Yuchens-MacBook-Pro.local>
2016-10-28 15:15:03 -0400

    task 4 done

--- cse303.p4/mylsl.c
+++ cse303.p4/mylsl.c
@@ -6,6 +6,9 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 /*
  * mylsl() - produce the appropriate directory listing(s)
@@ -22,22 +25,90 @@ int myCompare(const void * s1, const void *s2){
   // cast parameter to strings 
   char * str1 = *(char**)s1;
   char * str2 = *(char**)s2;
-
   comp = strcmp(str1,str2);
-
   return comp;
 }
 
+/** print file permission per file */
+void print_permission(char * link){
+    struct stat fileStat;
+    //printf("total: %4lld\n",fileStat.st_blocks);
+    if(stat(link,&fileStat)<0){
+        printf("file error.\n");exit(0);
+    }
+    // rwx permission
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("  ");
+
+    // print links 
+    printf("%3d  ",fileStat.st_nlink);
+
+    // owner name and group name
+    struct passwd *pw = getpwuid(fileStat.st_uid);
+    struct group  *gr = getgrgid(fileStat.st_gid);
+    printf("%10s ",pw->pw_name);
+    printf("%7s ",gr->gr_name);
+
+    // file size
+    printf("%6lld ",fileStat.st_size);
+
+    // print time
+    char date[20];
+    strftime(date, 20, "%b %d %H:%M ", localtime(&(fileStat.st_ctime)));
+    printf("%14s ",date);
+}
+
 /** print the list of files */
-void print_ls(char ** list,int n){
+void print_ls(char ** list,char * par_path, int n){
+  //printf("here in print_ls\n");
   int i;
+  int block_size = 0;
+  struct stat fileStat;
+    //printf("total: %4lld\n",fileStat.st_blocks);
   for(i=0;i<n;i++){
-    if(list[i][0]!='.'){ // if not a hidden file
-      if(line) printf("%s\n",list[i]); // new line 
-      else printf("%s\t ",list[i]); 
+
+    // concatenate file path
+    char * link = list[i];
+    char * newPath = malloc(strlen(par_path)+strlen(link)+2);
+    strcpy(newPath,par_path);
+    strcat(newPath,"/");
+    strcat(newPath,link);
+   
+    // sum up block size
+    if(list[i][0]!='.'){
+        //printf("list pass to block size:%s\n",newPath);
+        if(stat(newPath,&fileStat)<0){
+            printf("file error\n");exit(0);
+        }
+        block_size+=fileStat.st_blocks;
     }
   }
-  if(!line) printf("\n"); // new line at the end
+
+  printf("total %-4d\n",block_size);
+
+  // print permission 
+  for(i=0;i<n;i++){
+    char * link = list[i];
+    char * newPath = malloc(strlen(par_path)+strlen(link)+2);
+    strcpy(newPath,par_path);
+    strcat(newPath,"/");
+    strcat(newPath,link);
+
+    if(list[i][0]!='.'){ // if not a hidden file
+      //printf("list pass to permission:%s\n",newPath);
+      print_permission(newPath);  
+      printf("%s\n",newPath); // new line   
+    }
+  }
   printf("\n");
 }
 
@@ -66,9 +137,8 @@ void read_path(char * path){
     // sort as /bin/ls order 
     qsort(list_file,n,sizeof(char*),myCompare);
     // printf("after sort\n");
-
     if(num>1) printf("%s:\n",path);
-    print_ls(list_file,n);
+    print_ls(list_file,path,n);
   }
   for(i=0;i<n;i++) free(list_file[i]);
   free(list_file);
@@ -117,7 +187,7 @@ int main(int argc, char **argv) {
     /* parse the command-line options.  For this program, we only support  */
     /* the parameterless 'h' option, for getting help on program usage. */
     /* TODO: make sure to handle any other arguments specified by the assignment */
-    while ((opt = getopt(argc, argv, "h1R")) != -1) {
+    while ((opt = getopt(argc, argv, "h1l")) != -1) {
         switch(opt) {
           case 'h': help(argv[0]); break;
           case '1': line =1; break;
@@ -137,6 +207,7 @@ int main(int argc, char **argv) {
     for(;i<argc;i++){
       path[i-optind]=malloc(strlen(argv[i])+1);
       strcpy(path[i-optind],argv[i]);
+      //printf("%s\n",path[i-optind]);
     }
     num = argc - optind; // number of path
     // printf("%i\n",num);
